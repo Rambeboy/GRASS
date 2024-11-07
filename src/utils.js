cae78596e37baa3b21bc9fb1bc13597734d00cda
NODEBOY <kingindoslayer@gmail.com>
2024-11-07 11:54:11 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -5,9 +5,9 @@ const delay = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 function displayHeader() {
     process.stdout.write('\x1Bc');
     console.log('========================================'.cyan);
-    console.log('=        Grass Airdrop Bot - V2        ='.cyan);
-    console.log('=     Created by HappyCuanAirdrop      ='.cyan);
-    console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
+    console.log('=        GRASS AIRDROP BOT             ='.cyan);
+    console.log('=      Created by Nofan Rambe          ='.cyan);
+    console.log('=    https://github.com/Rambeboy       ='.cyan);
     console.log('========================================'.cyan);
     console.log();
   }
